50580bbfb596eb02f85f6b2405a4c4a997856629
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-02-09 23:42:30 -0300

    Rel um para muitos

--- src/main/java/teste/umparamuitos/ObterPedido.java
+++ src/main/java/teste/umparamuitos/ObterPedido.java
@@ -1,6 +1,7 @@
 package teste.umparamuitos;
 
 import infra.DAO;
+import modelo.umparamuitos.ItemPedido;
 import modelo.umparamuitos.Pedido;
 
 public class ObterPedido {
@@ -10,8 +11,13 @@ public class ObterPedido {
 		
 		DAO<Pedido> dao = new DAO<>(Pedido.class);
 		
-		Pedido pedido = dao.obterPorId(dao);
+		Pedido pedido = dao.obterPorId(1L);
 		
+		for(ItemPedido item : pedido.getItens()) {
+			System.out.println(item.toString());
+		}
+		
+		dao.fechar();
 		
 	}
 	
