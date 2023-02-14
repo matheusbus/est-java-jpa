7261394d118ef89f45c7278fcc30904f0964748f
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-02-13 23:44:29 -0300

    Nova NamedQuery

--- src/main/java/teste/consulta/ObterFilmes.java
+++ src/main/java/teste/consulta/ObterFilmes.java
@@ -11,8 +11,11 @@ public class ObterFilmes {
 	public static void main(String[] args) {
 		
 		DAO<Filme> dao = new DAO<>(Filme.class);
-		List<Filme> filmes = dao.consultar(
+		/*List<Filme> filmes = dao.consultar(
 						"obterFilmesComNotaMaiorQue", "nota", 8.5);
+		*/
+		List<Filme> filmes = dao.consultar(
+				"obterFilmesComNotaIgual", "nota", 7.5);
 		
 		for(Filme filme : filmes) {
 			System.out.println(filme.getNome());
