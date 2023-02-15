bb9b44049550adc1972e7d99aaa51f80447b0f05
Matheus Buschermoehle <matheusbuschermoehle@gmail.com>
2023-02-14 23:33:58 -0300

    Readme

--- src/main/java/teste/consulta/ObterMediaFilmes.java
+++ src/main/java/teste/consulta/ObterMediaFilmes.java
@@ -5,17 +5,14 @@ import modelo.consulta.NotaFilme;
 
 public class ObterMediaFilmes {
 
+	// Exemplo de named-native-query
+	
 	public static void main(String[] args) {
 		
 		DAO<NotaFilme> dao = new DAO<>(NotaFilme.class);
-		
 		NotaFilme nota = dao.consultarUm("obterMediaGeralDosFilmes");
-		
 		System.out.println("Média dos filmes utilizando named-native-query: " + nota.getMedia());
-		
 		dao.fechar();
-		
-		
 	}
 	
 }
